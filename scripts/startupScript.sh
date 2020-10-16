91210fe9e5b81e7585a916984a4d8540f414764f
Kim Ebert <kim@indicio.tech>
2020-10-16 13:13:50 -0600

    feat: fix script to run in bash
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- scripts/startupScript.sh
+++ scripts/startupScript.sh
@@ -2,7 +2,7 @@ CONTAINER_ALREADY_STARTED="/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    ./scripts/wait-for-it.sh localhost:3100 -s -t 60 -- npm run immeoid-setup
+    bash -c "./scripts/wait-for-it.sh localhost:3100 -s -t 60 -- npm run immeoid-setup"
 else
     echo "-- Not first container startup --"
     printenv command
